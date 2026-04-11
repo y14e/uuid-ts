526541effbee83da61438e2c4d143c7ab266a316
Yusuke Kamiyamane <y@y14e.com>
2026-04-11 21:55:09 +0900

    Delete uuid.js

--- uuid.js
+++
@@ -1,8 +0,0 @@
-export function generateUUID() {
-  if (typeof crypto.randomUUID === 'function') {
-    return crypto.randomUUID();
-  }
-  const uuid = URL.createObjectURL(new Blob()).slice(-36);
-  URL.revokeObjectURL(uuid);
-  return uuid;
-}
