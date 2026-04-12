99537efe4351205637f13144f0220754820e2969
Yusuke Kamiyamane <y@y14e.com>
2026-04-12 15:29:49 +0900

    Update uuid.ts

--- uuid.ts
+++ uuid.ts
@@ -5,7 +5,9 @@ export function generateUUID(): string {
     crypto.randomUUID?.() ??
     '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (char) => {
       crypto.getRandomValues(seed);
+
       const number = parseInt(char, 10);
+
       return (number ^ (seed[0] & (15 >> (number / 4)))).toString(16);
     })
   );
