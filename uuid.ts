f65de47c02a11fcb0c6c82267f0c472aa115e567
Yusuke Kamiyamane <y@y14e.com>
2026-04-12 15:16:00 +0900

    Update uuid.ts

--- uuid.ts
+++ uuid.ts
@@ -1,12 +1,11 @@
+const seed = new Uint32Array(1);
+
 export function generateUUID(): string {
-  if (typeof crypto.randomUUID === 'function') {
-    return crypto.randomUUID();
-  }
-
-  const url = URL.createObjectURL(new Blob());
-  const id = url.slice(-36);
-
-  URL.revokeObjectURL(url);
-
-  return id;
+  return typeof crypto.randomUUID === 'function'
+    ? crypto.randomUUID()
+    : '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (char) => {
+        crypto.getRandomValues(seed);
+        const number = parseInt(char, 10);
+        return (number ^ (seed[0] & (15 >> (number / 4)))).toString(16);
+      });
 }
